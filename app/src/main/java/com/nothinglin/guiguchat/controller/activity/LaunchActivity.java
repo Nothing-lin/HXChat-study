4b36901faa8e72a38d138352238d90c4bd6d706e
nothing-lin <1353695200@qq.com>
2022-07-20 21:11:50 +0800

    启动界面布局

---
+++ app/src/main/java/com/nothinglin/guiguchat/controller/activity/LaunchActivity.java
@@ -0,0 +1,17 @@
+package com.nothinglin.guiguchat.controller.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nothinglin.guiguchat.R;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_launch);
+    }
+}
