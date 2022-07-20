f1e85897098d55406a42ae0f225dc65832350a15
nothing-lin <1353695200@qq.com>
2022-07-20 20:54:37 +0800

    初始化环信SDK

--- app/src/main/java/com/nothinglin/guiguchat/IMApplication.java
+++ app/src/main/java/com/nothinglin/guiguchat/IMApplication.java
@@ -2,14 +2,32 @@ package com.nothinglin.guiguchat;
 
 import android.app.Application;
 
-public class IMApplication extends Application {
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
 
+public class IMApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initEasemob();
+    }
 
+    private void initEasemob(){
         //初始化环信SDK
+        EMOptions options = new EMOptions();
+        options.setAcceptInvitationAlways(false);//需要经过同意才能接受邀请
+        options.setAutoAcceptGroupInvitation(false);//需要同意后才能接受群邀请
+        // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+        options.setAutoTransferMessageAttachments(true);
+        // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+        options.setAutoDownloadThumbnail(true);
 
+
+        EMClient.getInstance().init(this,options);
+        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
     }
 }
