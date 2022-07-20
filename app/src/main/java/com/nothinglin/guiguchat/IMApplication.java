b5e9a6edaecec2d8e94798fecf436a3693f52c2d
nothing-lin <1353695200@qq.com>
2022-07-20 19:23:27 +0800

    初始化ａｐｐ项目

---
+++ app/src/main/java/com/nothinglin/guiguchat/IMApplication.java
@@ -0,0 +1,15 @@
+package com.nothinglin.guiguchat;
+
+import android.app.Application;
+
+public class IMApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //初始化环信SDK
+
+    }
+}
